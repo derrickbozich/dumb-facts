66e9a428f22f97cb9ef6e988f44a29dc80cb1954
Derrick <dbozich1@gmail.com>
2018-08-02 21:59:33 -0600

    created more scaffolding and began stubbing out routes

---
+++ app/controllers/facts_controller.rb
@@ -0,0 +1,29 @@
+class FactsController < Sinatra::Base
+  get '/:username/facts' do
+
+  end
+
+  get '/:username/facts/:id' do
+
+  end
+
+  post '/:username/facts/:id' do
+
+  end
+
+  get '/:username/facts/:id/edit' do
+
+  end
+
+  get '/facts' do
+
+  end
+
+  post '/facts' do
+
+  end
+
+  get '/facts/new' do
+
+  end
+end
