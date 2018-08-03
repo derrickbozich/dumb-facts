7ef4482c04544901ba86714128d67d4b8e6cf437
Derrick <dbozich1@gmail.com>
2018-08-02 22:17:03 -0600

    filled in some user_controller routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,34 @@
 class UsersController < Sinatra::Base
+  get '/login' do
+    erb :'users/login'
+  end
 
+  post 'login' do
+    @user = User.find_by(:username => params['username'])
+    if @user && @user.authenticate(params['password'])
+      session[:user_id] = @user.id
+    else
+      redirect '/login'
+    end
+    redirect '/facts'
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect '/login'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/signup' do
+    erb :'users/create_user'
+  end
+
+  post '/signup' do
+    
+  end
 
 
 
